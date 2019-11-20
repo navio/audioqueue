beb0bee87d7c875500bc451d362f224e0cd35b94
Alberto 🙈 <alnav@amazon.com>
2019-11-20 17:05:56 -0500

    Initial Commit with Basic functionality. AddNext, AddLast, Next, Play, Pause and AutoPlay

---
+++ AudioEngine.test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const AudioEngine = require("./dist/index.js").AudioEngine;
+
+const list = [{ title: "Audio", src: new URL("https://url.com")},{ title: "Audio1", src: new URL("https://url1.com")}]
+
+const mockPlayer = {
+    play: () => true,
+    pause: () => true,
+    addEventListener: () => true,
+}
+
+describe("AudioEngine should", () => {
+    it("should initialize with a playlist", () => {
+        const engine = new AudioEngine(list);
+        expect(engine.list.length === 2).toBe(true);
+    });
+    it("should reduce the playlist on next call", () => {
+        const engine = new AudioEngine(list, {audioObject: mockPlayer});
+        engine.next();
+        expect(engine.list.length === 1).toBe(true);
+    });
+    it("should chage autoplay", () => {
+        const engine = new AudioEngine(list, {audioObject: mockPlayer});
+        engine.autoplay = false;
+        expect(engine.autoplay).toBe(false);
+    });
+    it("should add new audio at the end of the queue", () => {
+        const engine = new AudioEngine(list, {audioObject: mockPlayer});
+        const newElement = { title: "newAudio", src: new URL("https://url.com/newaudio")};
+        engine.addLast(newElement);
+        expect(engine.list.length === 3).toBe(true);
+        expect( JSON.stringify(engine.list[engine.list.length -1])).toBe(JSON.stringify(newElement))
+    });
+    it("should add new audio at the begining of the queue", () => {
+        const engine = new AudioEngine(list, {audioObject: mockPlayer});
+        const newElement = { title: "newAudio", src: new URL("https://url.com/newaudio")};
+        engine.play();
+        engine.addNext(newElement);
+        expect(engine.list.length === 2).toBe(true);
+        expect( JSON.stringify(engine.list[0])).toBe(JSON.stringify(newElement))
+        engine.next();
+        expect(engine.list.length === 1).toBe(true);
+    });
+});
---
+++ AudioEngine.ts
@@ -0,0 +1,72 @@
+export interface IAudioParams {
+    image?: URL;
+    author?: String;
+    title: String;
+    src: URL;
+  }
+  
+export class AudioEngine {
+      private engine;
+      private queue = [];
+      private current: IAudioParams;
+      public autoplay: Boolean;
+      
+      constructor(init: IAudioParams[] = [], config: { audioObject?: AudioContext, autoplay?: boolean } = {}) {
+        this.queue = [...init];
+        this.engine = config.audioObject || new Audio();
+        this.autoplay = config.autoplay || true;
+        this.engine.addEventListener('ended',() => {
+          if( this.autoplay ) {
+            this.next();
+          }
+        });
+      }
+  
+      get list(): IAudioParams[] {
+        return this.queue;
+      }
+  
+      get audio() {
+        return this.engine;
+      }
+
+      get selected(){
+        return this.current;
+      }
+  
+      public play() {
+        if (this.current) {
+          this.engine.play();
+          return true;
+        } else {
+          return this.next();
+        }
+      }
+  
+      public next() {
+        if(!this.queue.length) {
+            return false;
+        }
+        this.current = this.queue.shift();
+  
+        if (this.current) {
+          this.engine.src = this.current.src;
+        }
+        this.engine.play();
+    
+        return true;
+      }
+  
+      public pause() {
+        this.engine.pause();
+        return true;
+      }
+  
+      public addLast(audio:IAudioParams) {
+        return this.queue.push(audio);
+      }
+  
+      public addNext(audio:IAudioParams) {
+        return this.queue.unshift(audio);
+      }
+  }
