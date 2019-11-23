a7dfe0995206f0ad2335ba386e3be431619d6b4d
Alberto 🙈 <alnav@amazon.com>
2019-11-23 18:25:12 -0500

    Adding Support for srcObject. ♺

--- AudioQueue.ts
+++ AudioQueue.ts
@@ -1,8 +1,7 @@
-export interface IAudioParams {
-  image?: URL;
-  author?: String;
-  title: String;
-  src: URL;
+interface IAudioParams {
+  [key: string]: any;
+  src?: String;
+  srcObject?: MediaStream | MediaSource | Blob;
 }
 
 class AudioQueueClass {
@@ -13,7 +12,7 @@ class AudioQueueClass {
 
   constructor(
     init: IAudioParams[] = [],
-    config: { audioObject?: HTMLElement; autoplay?: boolean } = {}
+    config: { audioObject?: HTMLElement; autoplay?: Boolean } = {}
   ) {
     this.queue = [...init];
     this.engine = config.audioObject || new Audio();
@@ -43,19 +42,33 @@ class AudioQueueClass {
   }
 
   public next() {
+
     if (!this.queue.length) {
       const event = new Event("EmptyQueue");
       this.engine.dispatchEvent(event);
       this.current = null;
       this.engine.pause();
-      this.engine.src = this.current.src;
+      this.engine.src = null;
+      this.engine.srcObject = null;
       return;
     }
+    
     this.current = this.queue.shift();
+
     if (this.current) {
-      this.engine.src = this.current.src;
+      const { src, srcObject } = this.current;
+
+      if(!src && !srcObject) {
+        const event = new Event("NoSource");
+        this.engine.dispatchEvent(event);
+        return this.next();
+      }
+
+      if (src) this.engine.src = src;
+      if (srcObject) this.engine.srcObject = srcObject
+      this.engine.play();
+
     }
-    this.engine.play();
   }
 
   public addLast(audio: IAudioParams) {
@@ -67,7 +80,7 @@ class AudioQueueClass {
   }
 
   public restart() {
-    return this.engine.current;
+    return this.engine.currentTime = 0;
   }
 }
 
